7ca8ee71d25c642b6e086cba506b25f14e4948ef
Saatvik Arya <aryasaatvik@gmail.com>
2026-03-14 20:20:13 +0530

    fix: align app todo api call

--- packages/app/src/context/sync.tsx
+++ packages/app/src/context/sync.tsx
@@ -528,7 +528,7 @@ export const { use: useSync, provider: SyncProvider } = createSimpleContext({
 
           const key = keyFor(directory, sessionID)
           return runInflight(inflightTodo, key, () =>
-            retry(() => client.session.todo.list({ sessionID })).then((todo) => {
+            retry(() => client.session.todo({ sessionID })).then((todo: { data?: any[] }) => {
               if (!tracked(directory, sessionID)) return
               const list = todo.data ?? []
               setStore("todo", sessionID, reconcile(list, { key: "id" }))
